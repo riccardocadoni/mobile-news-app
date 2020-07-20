844f5881421a0a46d03d1e85dddf11ccc36c84c3
Riccardo <cadoni.riccardo1@gmail.com>
2020-07-20 19:09:53 +0200

    fix: article visualizer content null crash

--- screens/ArticleVisualizer.tsx
+++ screens/ArticleVisualizer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet, ScrollView, View } from "react-native";
+import { StyleSheet, ScrollView, View, Text } from "react-native";
 import getRNDraftJSBlocks from "react-native-draftjs-render";
 //types
 import { CreatorContentType } from "../redux/contentSlice";
@@ -11,9 +11,19 @@ export interface ArticleVisualizerProps extends CreatorContentType {
 
 const ArticleVisualizer: React.SFC<ArticleVisualizerProps> = ({ route }) => {
   const { content } = route.params;
-  const contentState = JSON.parse(content);
+  if (!content)
+    return (
+      <View style={styles.container}>
+        <Text>Article not found</Text>
+      </View>
+    );
+  const contentState = content && JSON.parse(content);
   const blocks = getRNDraftJSBlocks({ contentState });
-  return <ScrollView style={styles.container}>{blocks}</ScrollView>;
+  return (
+    <View style={styles.container}>
+      <ScrollView>{blocks}</ScrollView>
+    </View>
+  );
 };
 
 export default ArticleVisualizer;
@@ -21,33 +31,7 @@ export default ArticleVisualizer;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  profileInfoContainer: {
-    flexDirection: "row",
-    backgroundColor: "#9FA8DA",
-    padding: 30,
-  },
-  textInfoContainer: { backgroundColor: "#9FA8DA" },
-  followingInfoContainer: {
-    flex: 1,
-  },
-  cardsContainer: {
-    flexDirection: "row",
-  },
-  text: {
-    fontSize: 20,
-    margin: 10,
-  },
-  imageProfile: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    margin: 20,
-  },
-  logOutBtn: {
+    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "orange",
-    padding: 10,
-    borderRadius: 10,
   },
 });
